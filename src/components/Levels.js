945b0aff8619ff547bdea9a36e5f1812b767b391
brahim mahious <brahim.mahious@hetic.net>
2022-06-12 23:46:03 +0200

    putting some styles

--- src/components/Levels.js
+++ src/components/Levels.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Stepper from 'react-stepper-horizontal'
 
 const Levels = ({levelNames, quizLevel}) => {
+
+  const [getstep, setGetStep] = useState([])
+
+  useEffect(() => {
+
+    const gettingSteps = levelNames.map(step =>{
+      return(
+        {title: step.toUpperCase()}
+      )
+    })
+
+ // En une seul ligne aussi : const gettingSteps = levelNames.map(step => ({title: step}))
+    setGetStep(gettingSteps)
+
+  }, [levelNames])
+
+  console.log(getstep)
+
+  
+
+
+
   return (
-    <div className='levelsContainer'>
-        <h2 className='headingLevels'> {levelNames[quizLevel]} </h2>
+    <div className='levelsContainer' style={{ background : 'transparent'}}>
+        {/* <h2 className='headingLevels'> {levelNames[quizLevel]} </h2> */}
+        
+          <Stepper 
+             steps={ getstep } 
+             activeStep={quizLevel} 
+             cirletop={0}
+             activeTitleColor= {'#d31017'}
+             activeColor= {'#d31017'}
+             completeTitleColor={'#E0E0E0'}
+             defaultTitleColor={'#E0E0E0'}
+             completeColor={'#E0E0E0'}
+             completeBarColor={'#E0E0E0'}
+             barStyle={'dashed'}
+             size={45}
+             circleFontSize={20}
+          />
+       
     </div>
   )
 }
 
-export default Levels
+export default React.memo(Levels)
+
+
+              
